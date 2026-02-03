c094fd764a0a416b98ffd0b15aade0ee72edff2f
Max Rakitin <mrakitin@bnl.gov>
2026-02-03 17:03:36 -0500

    Convert to use qtpy instead of PyQt5

--- isstools/widgets/widget_sample_manager.py
+++ isstools/widgets/widget_sample_manager.py
@@ -4,14 +4,14 @@ import numpy as np
 import pkg_resources
 import math
 
-from PyQt5 import uic, QtGui, QtCore, QtWidgets
-from PyQt5.QtGui import QPixmap, QCursor
-from PyQt5.Qt import QObject, Qt
-from PyQt5.QtCore import QThread, QSettings
-from PyQt5.QtWidgets import QMenu, QToolTip, QHBoxLayout, QWidget
+from qtpy import uic, QtGui, QtCore, QtWidgets
+from qtpy.QtGui import QPixmap, QCursor
+from qtpy.Qt import QObject, Qt
+from qtpy.QtCore import QThread, QSettings
+from qtpy.QtWidgets import QMenu, QToolTip, QHBoxLayout, QWidget
 from isstools.elements.widget_motors import UIWidgetMotors, UIWidgetMotorsWithSlider
 from ..elements.elements import remove_special_characters
-from PyQt5.QtWidgets import QLabel, QPushButton, QLineEdit, QSizePolicy, QSpacerItem
+from qtpy.QtWidgets import QLabel, QPushButton, QLineEdit, QSizePolicy, QSpacerItem
 import time as ttime
 from datetime import datetime
 from isstools.elements.qmicroscope import Microscope
@@ -100,7 +100,7 @@ sample_position_widget_dict = {
 
 
 class UISampleManager(*uic.loadUiType(ui_path)):
-    # sample_list_changed_signal = QtCore.pyqtSignal()
+    # sample_list_changed_signal = QtCore.Signal()
 
     def __init__(self,
                  sample_stage=None,
--- isstools/xasproject/scratch_pilatus.py
+++ isstools/xasproject/scratch_pilatus.py
@@ -1,9 +1,9 @@
 import pkg_resources
-from PyQt5 import uic, QtCore
+from qtpy import uic, QtCore
 from matplotlib.widgets import RectangleSelector, Cursor
-from PyQt5.Qt import QSplashScreen, QObject
-from PyQt5.QtWidgets import QToolTip
-from PyQt5.QtGui import QPixmap, QCursor
+from qtpy.Qt import QSplashScreen, QObject
+from qtpy.QtWidgets import QToolTip
+from qtpy.QtGui import QPixmap, QCursor
 from isstools.dialogs.BasicDialogs import message_box
 from isstools.elements.widget_motors import UIWidgetMotors
 from functools import partial
